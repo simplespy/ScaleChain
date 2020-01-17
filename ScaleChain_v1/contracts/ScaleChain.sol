5a23bd774418805dfa91c359ec5fc81842ec316a
Peiyao <psheng2@illinois.edu>
2020-01-17 13:53:08 -0600

    sendblock update

--- ScaleChain_v1/contracts/ScaleChain.sol
+++ ScaleChain_v1/contracts/ScaleChain.sol
@@ -5,11 +5,6 @@ contract ScaleChain {
     EtherNode[] public ether_Nodes;
     mapping(address => bool) is_etherNode;
     bytes32 curr_hash;
-    
-    struct SideChainBlock {
-        bytes[] transactions;
-        bytes32 signature;
-    }
 
     struct EtherNode {
         address id;
@@ -61,20 +56,6 @@ contract ScaleChain {
         returns (uint number_of_etherNodes) {
             number_of_etherNodes = ether_Nodes.length;
     }
-    
-    // Get block
-    function deserialize(bytes32 block_ser) public pure returns (SideChainBlock) {
-        //TODO: block deserialization
-        return SideChainBlock({
-            transactions: new bytes[](32),
-            signature: block_ser
-        });
-    }
-
-    function serialize(SideChainBlock side_chain_block) public pure returns (bytes32 block_ser) {
-        //TODO: block serialization
-        block_ser = side_chain_block.signature; 
-    }
 
     function splitSignature(bytes memory sig) internal pure returns (uint8 v, bytes32 r, bytes32 s) {
         require(sig.length == 65);
@@ -104,29 +85,27 @@ contract ScaleChain {
     }
     
     // compute hash
-    // TODO: add serialization & deserialization, param from side_chain_block+sig => block => block_ser;
-    function SendBlock(bytes memory side_chain_block, bytes memory sig) public returns (SendResult) {
-        // 0. deserialize to SideChainBlock
-        //SideChainBlock memory sidechainBlock = deserialize(block_ser);
+    function SendBlock(bytes memory tx_bytes, bytes memory sig) public returns (SendResult) {
         
         // 1.check signed by one of etherNode
-        // use recoverSigner(), more info https://solidity.readthedocs.io/en/v0.4.24/solidity-by-example.html#safe-remote-purchase
-        //bytes32 contents = prefixed(keccak256(abi.encodePacked(sidechainBlock.transactions, this)));
-        uint256 nonce = 100;
-        bytes32 contents = prefixed(keccak256(abi.encodePacked(side_chain_block, nonce, this)));
+        bytes32 contents = prefixed(keccak256(tx_bytes));
         address signer_address = recoverSigner(contents, sig);
 
         require (is_etherNode[signer_address]);
         
         // 2. update hash tentative codes
-        bytes32 h1 = sha256(abi.encodePacked(side_chain_block));
+        bytes32 h1 = sha256(abi.encodePacked(tx_bytes));
         bytes32 h2 = sha256(abi.encodePacked(curr_hash, h1));
         curr_hash = h2;
         
         // 3. publish the curr_hash as part of transaction   
-        // use address.call() more info https://solidity.readthedocs.io/en/latest/units-and-global-variables.html#mathematical-and-cryptographic-functions
         bool publish_result = address(this).call();
         return SendResult({result: publish_result, tx_hash: curr_hash});
-    } 
+    }
+
+    function TestSignerAddress(bytes memory tx_bytes, bytes memory sig) public returns (address signer_address) {
+        bytes32 contents = prefixed(keccak256(abi.encodePacked(tx_bytes)));
+        signer_address = recoverSigner(contents, sig);
+    }
 
 }
