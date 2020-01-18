2ec19feb6848b2e43db36be2fb47021c05f4da4a
Peiyao <psheng2@illinois.edu>
2020-01-17 19:20:09 -0600

    phase1-test

--- ScaleChain_v1/contracts/ScaleChain.sol
+++ ScaleChain_v1/contracts/ScaleChain.sol
@@ -2,11 +2,11 @@ pragma solidity ^0.4.24;
 pragma experimental ABIEncoderV2;
 
 contract ScaleChain {
-    EtherNode[] public ether_Nodes;
-    mapping(address => bool) is_etherNode;
+    MainNode[] public main_nodes;
+    mapping(address => bool) is_mainNode;
     bytes32 curr_hash;
 
-    struct EtherNode {
+    struct MainNode {
         address id;
         bytes32 pubkey;
     }
@@ -16,34 +16,34 @@ contract ScaleChain {
         bytes32 tx_hash; //maybe useful
     }
 
-    // Initialize EtherNodes so that a sender of a new block
-    // has to be one of the etherNode
-    constructor(address[] ether_node_addresses) public {
-        for (uint i = 0; i < ether_node_addresses.length; ++i) {
-            ether_Nodes.push(EtherNode({
-                id: ether_node_addresses[i],
+    // Initialize MainNodes so that a sender of a new block
+    // has to be one of the mainNode
+    constructor(address[] main_node_addresses) public {
+        for (uint i = 0; i < main_node_addresses.length; ++i) {
+            main_nodes.push(MainNode({
+                id: main_node_addresses[i],
                 pubkey: 0
             }));
-            is_etherNode[ether_node_addresses[i]] = true;
+            is_mainNode[main_node_addresses[i]] = true;
         }
     }
 
-    // Current EtherNode authorize new EtherNode
+    // Current MainNode authorize new MainNode
     // Note: check msg.sender and not duplicate
-    function addEtherNode(address new_ether_node) public {
-        require(is_etherNode[msg.sender]);
-        require(!is_etherNode[new_ether_node]);
-        ether_Nodes.push(EtherNode({
-            id: new_ether_node,
+    function addMainNode(address new_main_node) public {
+        require(is_mainNode[msg.sender]);
+        require(!is_mainNode[new_main_node]);
+        main_nodes.push(MainNode({
+            id: new_main_node,
             pubkey: 0
         }));
-        is_etherNode[new_ether_node] = true;
+        is_mainNode[new_main_node] = true;
     }
 
     // Get Ether node addresses
-    function getEtherNodes(uint id) public view
-        returns (address ether_node_addresses) {
-            ether_node_addresses = ether_Nodes[id].id;
+    function getMainNodes(uint id) public view
+        returns (address main_node_address) {
+            main_node_address = main_nodes[id].id;
     }
 
     function getCurrentHash() public view
@@ -52,9 +52,9 @@ contract ScaleChain {
     }
 
     // Get # of Ether nodes
-    function etherNodesCount() public view
-        returns (uint number_of_etherNodes) {
-            number_of_etherNodes = ether_Nodes.length;
+    function mainNodesCount() public view
+        returns (uint number_of_mainNodes) {
+            number_of_mainNodes = main_nodes.length;
     }
 
     function splitSignature(bytes memory sig) internal pure returns (uint8 v, bytes32 r, bytes32 s) {
@@ -64,48 +64,34 @@ contract ScaleChain {
             s := mload(add(sig, 64))
             v := byte(0, mload(add(sig, 96)))
         }
-    }
-
-    function recoverSigner(bytes32 contents, bytes memory signature) internal pure returns (address signer_address) {
-        
-        (uint8 v, bytes32 r, bytes32 s) = splitSignature(signature);
-        signer_address = ecrecover(contents, v, r, s);
-    }
-
-    function prefixed(bytes32 hash) internal pure returns (bytes32) {
-        return keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", hash));
-    }
-    
-    function toBytes(bytes32 _data) internal pure returns (bytes memory b) {
-        b = new bytes(_data.length);
-        for(uint i = 0; i < _data.length; ++i) {
-            b[i] = _data[i];
-        }
-        return b;
+        v = v + 27;
     }
     
     // compute hash
-    function SendBlock(bytes memory tx_bytes, bytes memory sig) public returns (SendResult) {
+    function SendBlock(string tx_bytes, bytes memory sig) public returns (SendResult) {
         
-        // 1.check signed by one of etherNode
-        bytes32 contents = prefixed(keccak256(tx_bytes));
-        address signer_address = recoverSigner(contents, sig);
+        // 1.check signed by one of mainNode
+        address signer_address = recoverSigner(tx_bytes, sig);
 
-        require (is_etherNode[signer_address]);
+        require (is_mainNode[signer_address]);
         
         // 2. update hash tentative codes
-        bytes32 h1 = sha256(abi.encodePacked(tx_bytes));
-        bytes32 h2 = sha256(abi.encodePacked(curr_hash, h1));
-        curr_hash = h2;
+        bytes32 new_hash = sha256(abi.encodePacked(curr_hash, sha256(tx_bytes)));
+        curr_hash = new_hash;
         
         // 3. publish the curr_hash as part of transaction   
         bool publish_result = address(this).call();
         return SendResult({result: publish_result, tx_hash: curr_hash});
     }
 
-    function TestSignerAddress(bytes memory tx_bytes, bytes memory sig) public returns (address signer_address) {
-        bytes32 contents = prefixed(keccak256(abi.encodePacked(tx_bytes)));
-        signer_address = recoverSigner(contents, sig);
+    function recoverSigner(string tx_bytes, bytes memory sig) returns (address signer_address) {
+        bytes memory prefix = "\x19Ethereum Signed Message:\n32";
+        bytes32 prefixedHash = sha3(prefix, sha3(tx_bytes));
+        (uint8 v, bytes32 r, bytes32 s) = splitSignature(sig);
+        signer_address = ecrecover(prefixedHash, v, r, s);
     }
+
+
+    
 
 }
--- ScaleChain_v1/test/test_phase1.js
+++ ScaleChain_v1/test/test_phase1.js
@@ -3,27 +3,15 @@ var ScaleChain = artifacts.require("./ScaleChain.sol");
 
 
 
-function signBlock(contents, nonce, contractAddress, callback) {
-    var hash = "0x" + web3.utils.soliditySha3(
-        {t: "bytes32", v: contents}, 
-        {t: "uint256", v:nonce}, 
-        {t: 'address', v: contractAddress}
-    ).toString("hex");
-
-    web3.eth.sign(hash, web3.eth.defaultAccount, callback);
-      
+function signBlock(tx_bytes, signer) {
+    var hash = web3.utils.soliditySha3(tx_bytes);
+    return web3.eth.sign(hash, signer).then(token => { return token });      
 }
-
-// hash = soliditySha3(transactions + contractAddress)
-// sig = sign(hash, signer's key)
-// block = {transactions, sig}
-// block_ser = ser(block)
-
 
 contract("ScaleChain", function(accounts) {
     it ("Test 1: initializes with accounts", function() {
         return ScaleChain.deployed().then(function(instance) {
-            return instance.etherNodesCount();
+            return instance.mainNodesCount();
         }).then(function(count) {
             assert.equal(count.toNumber(), 5);
         });
@@ -32,20 +20,18 @@ contract("ScaleChain", function(accounts) {
     it("Test 2: add account", function() {
         ScaleChain.deployed().then(function(instance) {
             web3.eth.defaultAccount = accounts[0];
-            instance.addEtherNode(accounts[5]);
-            return instance.etherNodesCount();
+            instance.addMainNode(accounts[5]);
+            return instance.mainNodesCount();
         }).then(function(count) {
             assert.equal(count.toNumber(), 6);
         });
     });
 
-    it("Test 3: signing blocks", function() {
-        return ScaleChain.deployed().then(function(instance) {
-            web3.eth.defaultAccount = accounts[0];
-            const block = "";//new ArrayBuffer(32);
-            signBlock(block, 100, instance.address, function(err, sig) {
-                assert.equal(instance.SendBlock(block, sig), web3.eth.defaultAccount);
-            });
-        });
+    it("Test 3: signing blocks", async function() {
+        var instance = await ScaleChain.deployed();
+        var tx_bytes = "deadbeef";
+        var sig = await signBlock(tx_bytes, accounts[0]);
+        var result = await instance.recoverSigner.call(tx_bytes, sig);
+        assert.equal(result, accounts[0]);
     });
 });
