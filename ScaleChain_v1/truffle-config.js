bb6a6e8db006b3f9048779d3153703b0d54a3296
psheng2 <psheng2@illinois.edu>
2020-01-24 14:28:44 -0600

    ropsten-config

--- ScaleChain_v1/truffle-config.js
+++ ScaleChain_v1/truffle-config.js
@@ -1,36 +1,19 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+const MNEMONIC = 'practice dawn lamp foot pumpkin blame imitate atom robot culture ride toss';
+
 module.exports = {
-  // Uncommenting the defaults below 
-  // provides for an easier quick-start with Ganache.
-  // You can also follow this format for other networks;
-  // see <http://truffleframework.com/docs/advanced/configuration>
-  // for more details on how to specify configuration options!
-  //
   networks: {
     development: {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*"
-    },
-  //  test: {
-  //    host: "127.0.0.1",
-  //    port: 7545,
-  //    network_id: "*"
-  //  }
   },
-  //
-  compilers: {
-    solc: {
-      version: "^0.4.24", // A version or constraint - Ex. "^0.5.0"
-                         // Can also be set to "native" to use a native solc
-      //docker: <boolean>, // Use a version obtained through docker
-      //parser: "solcjs",  // Leverages solc-js purely for speedy parsing
-      //settings: {
-       /// optimizer: {
-         // enabled: <boolean>,
-          //runs: <number>   // Optimize for how many times you intend to run the code
-        //},
-        //evmVersion: <string> // Default: "petersburg"
-      //}
+  ropsten: {
+      provider: function() {
+        return new HDWalletProvider(MNEMONIC, "https://ropsten.infura.io/v3/b242a89f988c41fdb40a6d3ebf724334")
+      },
+      network_id: 3,
+      gas: 4000000
     }
   }
 };
